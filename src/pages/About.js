1a87bdd37394fb034ba586d248ff1d226a007143
Evelyn Cheng <evelyncheng2003@gmail.com>
2024-06-11 00:26:34 -0700

    Initial commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import EvelynPic from "../assets/evelyn.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                src={EvelynPic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Evelyn Cheng</div>
+              <div className="brief_description">
+                I am a rising junior at Columbia University majoring in Computer Science and minoring in Economics!
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
